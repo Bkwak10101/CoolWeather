262870b92463e55eb267de7d25db35235a361a86
Bkwak10101 <bartekkwak@gmail.com>
2023-08-06 18:28:22 +0200

    Added WeatherCode data

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -11,6 +11,8 @@ export class DashboardComponent implements OnInit {
   data: any;
   forecast: Record<string, any> = {};
 
+  weatherMappings: { range: number[]; imagePath: string }[] = [];
+
   constructor(private weatherClientService: WeatherClientService) {
   }
 
@@ -22,11 +24,29 @@ export class DashboardComponent implements OnInit {
 
         let times: any = this.data['hourly']['time'];
         let temperatures: any = this.data['hourly']['temperature_2m'];
+        let daily: any = this.data['daily']['time'];
+        let code: any = this.data['daily']['weathercode'];
 
         this.forecast['temperature'] = this.weatherClientService.getCurrentData(times, temperatures);
         this.forecast['temperatureTomorrow'] = this.weatherClientService.getTomorrowData(times, temperatures);
         this.forecast['temperatureAfterTomorrow'] = this.weatherClientService.getDayAfterTomorrowData(times, temperatures);
+        this.forecast['currentWeather'] = this.weatherClientService.getCurrentWeatherCode(daily, code)
+        this.forecast['tomorrowWeather'] = this.weatherClientService.getWeatherForDay(daily, code, 1);
+        this.forecast['afterTomorrowWeather'] = this.weatherClientService.getWeatherForDay(daily, code, 2);
+
+        this.setupWeatherMappings();
+
       })
     ;
+  }
+  private setupWeatherMappings(): void {
+    this.weatherMappings = [
+      { range: [0], imagePath: 'assets/images/sun.png' },
+      { range: [1, 2, 3], imagePath: 'assets/images/clear-sky.png' },
+      { range: Array.from({ length: 60 }, (_, i) => i + 4), imagePath: 'assets/images/rain.png' },
+      { range: Array.from({ length: 7 }, (_, i) => i + 64), imagePath: 'assets/images/cloudy.png' },
+      { range: Array.from({ length: 16 }, (_, i) => i + 71), imagePath: 'assets/images/snow.png' },
+      { range: Array.from({ length: 13 }, (_, i) => i + 87), imagePath: 'assets/images/storm.png' },
+    ];
   }
 }
--- src/app/services/weather-client.service.ts
+++ src/app/services/weather-client.service.ts
@@ -11,7 +11,7 @@ export class WeatherClientService {
   private data: any;
 
   constructor(private httpClient: HttpClient) {
-    this.url = "https://api.open-meteo.com/v1/forecast?latitude=50.06&longitude=19.94&hourly=temperature_2m";
+    this.url = "https://api.open-meteo.com/v1/forecast?latitude=50.06&longitude=19.94&hourly=temperature_2m&daily=weathercode&timezone=auto";
     this.data = this.httpClient.get(this.url);
   }
 
@@ -22,6 +22,9 @@ export class WeatherClientService {
   getCurrentData(dates: Date[], data: any[]) {
     const currentTime = new Date().getHours();
     let index = 0;
+
+    console.log(data)
+
     for (const date in dates) {
       if (date == currentTime.toString()) {
         return data[index];
@@ -43,7 +46,6 @@ export class WeatherClientService {
       index++;
     }
     return data[index];
-
   }
 
   getDayAfterTomorrowData(dates: Date[], data: any[]) {
@@ -52,12 +54,49 @@ export class WeatherClientService {
 
     for (const date in dates) {
       if (date == tomorrowTime.toString()) {
-
         return data[index];
       }
       index++;
     }
     return data[index];
+  }
+
+  getCurrentWeatherCode(dates: string[], code: any[]) {
+    const currentDay = new Date().getDate();
+    const formattedDate = this.formatDate(dates[0]);
+    let index = 0;
+
+    for (const date in dates) {
+      const formattedDate = this.formatDate(dates[date]);
+      if (formattedDate == currentDay) {
+        return code[index];
+      }
+      index++;
+    }
+
+    return code;
+  }
+
+  getWeatherForDay(dates: string[], code: any[], dayOffset: number) {
+    const currentDate = new Date();
+    currentDate.setDate(currentDate.getDate() + dayOffset);
+    const targetDate = currentDate.getDate();
+    let index = 0;
+
+    for (const date in dates) {
+      const formattedDate = this.formatDate(dates[date]);
+      if (formattedDate === targetDate) {
+        return code[index];
+      }
+      index++;
+    }
+
+    return code;
+  }
+
+  formatDate(dateStr: string): number {
+    const date = new Date(dateStr);
+    return date.getDate();
   }
 }
 
